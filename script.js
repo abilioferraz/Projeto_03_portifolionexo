d22a11f54a6a248b1be28cc9792b63c090fcbe44
Abilio Ferraz <a.juniorferraz@gmail.com>
2026-01-02 12:41:51 -0300

    Atualização de paginas

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// MENU MOBILE
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 
@@ -9,4 +10,17 @@ document.querySelectorAll('.menu a').forEach(link => {
   link.addEventListener('click', () => {
     menu.classList.remove('active');
   });
+});
+
+// CARROSSEL
+const carousel = document.querySelector('.carousel');
+const btnLeft = document.querySelector('.carousel-btn.left');
+const btnRight = document.querySelector('.carousel-btn.right');
+
+btnRight.addEventListener('click', () => {
+  carousel.scrollBy({ left: 325, behavior: 'smooth' });
+});
+
+btnLeft.addEventListener('click', () => {
+  carousel.scrollBy({ left: -325, behavior: 'smooth' });
 });
